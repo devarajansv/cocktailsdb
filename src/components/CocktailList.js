4d700da7bbbaacb06283b35a2a52d36069c1aa9f
devarajan <rajan.deva85@gmail.com>
2020-08-28 21:00:09 +0530

    Init commit

---
+++ src/components/CocktailList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Cocktail from "./Cocktail";
+
+export default function CocktailList({ cocktails, loading }) {
+  if (loading) {
+    return <h1 className="section-title">Loading...</h1>;
+  }
+  if (cocktails.length < 1) {
+    return (
+      <h2 className="section-title">
+        No cocktails matched your search criteria
+      </h2>
+    );
+  }
+
+  return (
+    <section className="section">
+      <h2 className="section-title">cocktails</h2>
+      <div className="cocktails-center">
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />;
+        })}
+      </div>
+    </section>
+  );
+}
