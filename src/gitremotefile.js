9079dfa4915f26979ba81ae1b7f9a6ddbdf07799
devarajan <devarajan.sv85@gmail.com>
2020-10-11 14:24:49 +0530

    git remote repo

---
+++ src/gitremotefile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const gitremotefile = props => {
+    return (
+        <div>
+            Hello from git learning
+        </div>
+    )
+}
+
+gitremotefile.propTypes = {
+
+}
+
+export default gitremotefile
