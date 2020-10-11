a687bf24f829dacde8d20e6092b86ee53d617061
devarajan <devarajan.sv85@gmail.com>
2020-10-11 14:32:34 +0530

    merging with remote repo

---
+++ src/gitmerge.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const gitmerge = props => {
+    return (
+        <div>
+            hellow from merging 
+        </div>
+    )
+}
+
+gitmerge.propTypes = {
+
+}
+
+export default gitmerge
